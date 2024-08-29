b5fe7eec8b8bdc425b34e25b08f23acb53beb8ce
SoraTenshi <dream@neoncity.dev>
2024-08-30 01:59:07 +0200

    Update

--- build.zig
+++ build.zig
@@ -2,11 +2,11 @@ const std = @import("std");
 const zine = @import("zine");
 
 pub fn build(b: *std.Build) !void {
-    try zine.addWebsite(b, .{
-        .layouts_dir_path = "layouts",
-        .content_dir_path = "content",
-        .static_dir_path = "static",
+    zine.website(b, .{
         .title = "Blog by SoraNoTenshi",
         .host_url = "https://neoncity.dev/",
+        .layouts_dir_path = "layouts",
+        .content_dir_path = "content",
+        .assets_dir_path = "assets",
     });
 }
