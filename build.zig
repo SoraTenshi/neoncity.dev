389bdc7597e62f8a1a92a728cb2b1f332e3d6d53
SoraTenshi <dream@neoncity.dev>
2024-10-06 04:06:39 +0200

    big migration change

--- build.zig
+++ build.zig
@@ -8,5 +8,22 @@ pub fn build(b: *std.Build) !void {
         .layouts_dir_path = "layouts",
         .content_dir_path = "content",
         .assets_dir_path = "assets",
+        .static_assets = &.{
+            "CNAME",
+            "Lilex/lilex-regular.woff2",
+            "palette.css",
+            "fonts.css",
+        },
+        .build_assets = &.{
+            .{
+                .name = "zon",
+                .lp = b.path("build.zig.zon"),
+            },
+            .{
+                .name = "frontmatter",
+                .lp = b.dependency("zine", .{}).path("fontmatter.ziggy-scheme"),
+            },
+        },
+        .debug = true,
     });
 }
