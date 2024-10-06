6c19f588bc2bbb6bbf0ec8789705ad5b573c7891
Sora <dream@neoncity.dev>
2024-10-06 04:07:47 +0200

    Migrate to 0.6.3 (#6)
    * Update
    
    * big migration change

--- build.zig
+++ build.zig
@@ -2,11 +2,28 @@ const std = @import("std");
 const zine = @import("zine");
 
 pub fn build(b: *std.Build) !void {
-    try zine.addWebsite(b, .{
-        .layouts_dir_path = "layouts",
-        .content_dir_path = "content",
-        .static_dir_path = "static",
+    zine.website(b, .{
         .title = "Blog by SoraNoTenshi",
         .host_url = "https://neoncity.dev/",
+        .layouts_dir_path = "layouts",
+        .content_dir_path = "content",
+        .assets_dir_path = "assets",
+        .static_assets = &.{
+            "CNAME",
+            "Lilex/lilex-regular.woff2",
+            "palette.css",
+            "fonts.css",
+        },
+        .build_assets = &.{
+            .{
+                .name = "zon",
+                .lp = b.path("build.zig.zon"),
+            },
+            .{
+                .name = "frontmatter",
+                .lp = b.dependency("zine", .{}).path("fontmatter.ziggy-scheme"),
+            },
+        },
+        .debug = true,
     });
 }
